239f9f11c363f2448c062a373316e1e48f21a36f
Victoria Nuñez <vickylucianuniez@gmail.com>
2019-11-14 12:38:27 -0300

    primera version

---
+++ tp2.js
@@ -0,0 +1,25 @@
+const vendedoras = ["Ada", "Grace", "Hedy", "Sheryl"];
+
+const ventas = [
+    [ 100, 4, 2, 2019, 'Grace', 'Centro', ['Monitor GPRS 3000', 'Motherboard ASUS 1500']],
+    [ 101, 1, 1, 2019, 'Ada', 'Centro', ['Monitor GPRS 3000', 'Motherboard ASUS 1500']],
+    [ 102, 2, 1, 2019, 'Grace', 'Caballito', ['Monitor ASC 543', 'Motherboard MZI', 'HDD Toyiva']],
+    [ 103, 10, 1, 2019, 'Ada', 'Centro', ['Monitor ASC 543', 'Motherboard ASUS 1200']],
+    [ 104, 12, 1, 2019, 'Grace', 'Caballito', ['Monitor GPRS 3000', 'Motherboard ASUS 1200']],
+    [ 105, 21, 3, 2019, 'Hedy', 'Caballito', ['Monitor ASC 543', 'Motherboard ASUS 1200', 'RAM Quinston']]
+    ];
+
+const precios = [ 
+    [ 'Monitor GPRS 3000', 200  ],
+    [ 'Motherboard ASUS 1500', 120  ],
+    [ 'Monitor ASC 543', 250  ],
+    [ 'Motherboard ASUS 1200', 100  ],
+    [ 'Motherboard MZI', 30  ],
+    [ 'HDD Toyiva', 90  ],
+    [ 'HDD Wezter Dishital', 75  ],
+    [ 'RAM Quinston', 110  ],
+    [ 'RAM Quinston Fury', 230  ]
+];
+
+const sucursales = [ 'Centro', 'Caballito' ];
+
